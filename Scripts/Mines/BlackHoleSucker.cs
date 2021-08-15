96ffb828c349138ea02194cd79e9c08cdd499e0a
JatinMunjal2k <jatinmunjal2k@gmail.com>
2021-08-15 15:44:40 +0530

    First Commit
    1) homing missile
    2) black hole sucker in progress

---
+++ Scripts/Mines/BlackHoleSucker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BlackHoleSucker : MonoBehaviour
+{
+    public float myRadius = 1.0f;
+    public float myStrength = 0.1f;
+
+    void Update()
+    {
+        GameObject player = GameManager.instance.player;
+        if(player == null || Vector3.Distance(player.transform.position, transform.position) > myRadius) {
+            return;
+        }
+
+        player.transform.position += myStrength * (transform.position - player.transform.position) / Vector3.Distance(player.transform.position, transform.position);
+    }
+}
---
+++ Scripts/ShootingProjectiles/Homing.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A class to make projectiles move
+/// </summary>
+public class Homing : MonoBehaviour
+{
+    [Tooltip("The distance this projectile will move each second.")]
+    public float projectileSpeed = 3.0f;
+    public float minAngleToRotate = 5.0f;
+
+    private GameObject enemy = null;
+    private float targetRadius = 10.0f;
+
+    private void Start()
+    {
+        EnemyToTarget();
+    }
+
+    /// <summary>
+    /// Description:
+    /// Standard Unity function called once per frame
+    /// Inputs: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    private void Update()
+    {
+        MoveProjectile();
+    }
+
+    /// <summary>
+    /// Description:
+    /// Move the projectile in the direction it is heading
+    /// Inputs: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    private void MoveProjectile()
+    {
+        if (enemy == null || !enemy.activeInHierarchy) {
+            EnemyToTarget();
+        }
+
+        var directionToMove = (enemy == null) ? transform.up : (enemy.transform.position - transform.position);
+
+        var angle = Vector3.Angle(transform.up, directionToMove);
+        transform.Rotate(0, 0, Mathf.Min(angle, minAngleToRotate));
+
+        // move the transform
+        transform.position = transform.position + transform.up * projectileSpeed * Time.deltaTime;
+    }
+
+    private void EnemyToTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject localEnemy in enemies) {
+            if (Vector3.Distance(localEnemy.transform.position, transform.position) <= targetRadius) {
+                enemy = localEnemy;
+                return;
+            }
+        }
+    }
+}
